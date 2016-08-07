c13d51f196ba5ce257a7dd6858946ff13a7ec4de
Ivan Ivankovic <ivankoi@gmail.com>
2016-08-07 20:23:33 +0200

    Added controller integration test

---
+++ src/test/java/com/ivankoi/home/HomeControllerIT.java
@@ -0,0 +1,53 @@
+package com.ivankoi.home;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.inject.Inject;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by ivankoi on 8/7/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "file:**/WEB-INF/dynamicTilesWeb-servlet.xml")
+@WebAppConfiguration
+public class HomeControllerIT {
+
+    @Inject
+    private WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void springMvcProperlyConfigured() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(forwardedUrl("/WEB-INF/view/jsp/home.jsp"));
+
+
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(forwardedUrl("/WEB-INF/view/jsp/home.jsp"));
+    }
+
+
+}
